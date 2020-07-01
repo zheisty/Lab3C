e1fddcbb2e772a931b337c940f80db80487fba8c
zheisty <S01253938@acad.tri-c.edu>
2020-07-01 00:36:22 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Diagnostics.Tracing;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double remain;
+            int nP = 0;
+
+            Console.WriteLine("Enter Number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int prime = 2; prime < number; prime++)
+            {
+
+                    remain = number % prime;
+                    if (remain == 0) 
+                    {
+                        Console.WriteLine("The Number " + number + " is Not Prime");
+                        nP++;
+                    break;
+                    }
+
+                if (nP > 0)
+                {
+                    Console.WriteLine("The Number is " + number + " is Not Prime");
+                }
+                else 
+
+                { 
+                    Console.WriteLine("The Number is" + number + " is Prime");
+                }
+
+                   
+            }
+        }
+    }
+}
